97592f23c67d1c2622c05c759b9a608a1188f3b0
Jayant Dabas <jayantd1112@gmail.com>
2025-11-28 13:15:17 -0500

    add cli arguments

---
+++ configs/dataset.py
@@ -0,0 +1,16 @@
+DATA_PARTS = [
+    'cc3m_96px_part1.zip',
+    'cc3m_96px_part2.zip',
+    'cc3m_96px_part3.zip',
+    'cc3m_96px_part4.zip',
+    'cc3m_96px_part5.zip',
+]
+
+# Provide real SHA256 hashes here
+CHECKSUMS = {
+    'cc3m_96px_part1.zip': '276b6760b5acb4b72131353e6f7fd334ef8422e0d6b21198bcd647c7b9560819',
+    'cc3m_96px_part2.zip': '888874f0609f6619d59dbf97d69a28ebb4538997c5f9d89822a173b1faef83e1',
+    'cc3m_96px_part3.zip': '5634f55e9cea8aab85e40182785c0ff728cbcd331f7cdf5325e6d60d4452d330',
+    'cc3m_96px_part4.zip': 'a098f8ca79a245147c09cfe4d1c80e4d6f65a0985ab3d29bab940d9824c5cd29',
+    'cc3m_96px_part5.zip': '60a8931d3243aecd0ab502fd1b2ca987b1ea490df69e80e16b5a2d7019a180d0',
+}

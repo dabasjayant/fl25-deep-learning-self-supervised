c4c3791821b5cc60a9198662fb01b89f0a4d9f36
Jayant Dabas <jayantd1112@gmail.com>
2025-11-29 16:04:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,12 @@ from src.data.ingestion.downloader import DatasetDownloader
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Self-supervised Learning')
-    parser.add_argument(
-        '--download', '-d',
-        action='store_true',
-        help='Download datset from hugging face.'
-    )
+    parser.add_argument( '--download', '-d', action='store_true', help='Download datset from hugging face.')
     args = parser.parse_args()
 
     if args.download:
         parts = DATA_PARTS
         checksums = CHECKSUMS
-
         downloader = DatasetDownloader(
             repo_id='tsbpp/fall2025_deeplearning',
             parts=parts,
@@ -24,5 +19,4 @@ if __name__ == '__main__':
             checksums=checksums,
             remove_zip_after_extract=True
         )
-
         downloader.download_and_prepare()
